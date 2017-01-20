22f63db5d44da933069ee98dedfed2d3e74075e2
Yanpei Li <liyanpei.jack@gmail.com>
2017-01-19 20:18:52 -0500

    minify css files

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp');
 var ngAnnotate = require('gulp-ng-annotate');
 var uglify = require('gulp-uglify');
 var optimizejs = require('gulp-optimize-js');
+var concat = require('gulp-concat');
+var cleanCss = require('gulp-clean-css');
 
-gulp.task('default', ['buildJS'], function () {
+gulp.task('default', ['buildJS', 'buildCSS'], function () {
 });
 
 gulp.task('buildJS', function () {
@@ -11,13 +13,13 @@ gulp.task('buildJS', function () {
         .pipe(ngAnnotate())
         .pipe(uglify())
         .pipe(optimizejs())
-        .pipe(gulp.dest('static/dist/javascripts/'));
+        .pipe(gulp.dest('dist/javascripts/'));
 });
 
 gulp.task('buildCSS', function () {
     return gulp.src('static/css/**/*.css')
-        .pipe(ngAnnotate())
-        .pipe(uglify())
-        .pipe(optimizejs())
-        .pipe(gulp.dest('static/dist/css/'));
+        .pipe(concat('stylesheet.css'))
+        .pipe(cleanCss())
+        .pipe(gulp.dest('dist/stylesheet'));
 });
+
