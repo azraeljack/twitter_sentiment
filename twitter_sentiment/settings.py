2f47b67b165b190c0a7e09576ed005b5d8bb778b
Yanpei Li <liyanpei.jack@gmail.com>
2017-01-19 22:13:37 -0500

    fix run.py cannot find file issue

--- twitter_sentiment/settings.py
+++ twitter_sentiment/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kd@5#!n-2*1x5$vevfk)=6-n7w=nxkz%kr+hii&x@-4!9)5*g#'
+SECRET_KEY = 'your-secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
