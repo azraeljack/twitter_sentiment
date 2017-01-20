066c2c2ca0169929d4f2ccbad9740084ea0dae35
Yanpei Li <liyanpei.jack@gmail.com>
2017-01-19 19:41:00 -0500

    added optimization js task and update license

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 var gulp = require('gulp');
 var ngAnnotate = require('gulp-ng-annotate');
 var uglify = require('gulp-uglify');
+var optimizejs = require('gulp-optimize-js');
 
-gulp.task('default', ['build'], function () {
+gulp.task('default', ['buildJS'], function () {
 });
 
-gulp.task('build', function () {
-  return gulp.src('static/javascripts/**/*.js')
-    .pipe(ngAnnotate())
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/static/javascripts/'));
+gulp.task('buildJS', function () {
+    return gulp.src('static/javascripts/**/*.js')
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(optimizejs())
+        .pipe(gulp.dest('static/dist/javascripts/'));
+});
+
+gulp.task('buildCSS', function () {
+    return gulp.src('static/css/**/*.css')
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(optimizejs())
+        .pipe(gulp.dest('static/dist/css/'));
 });
